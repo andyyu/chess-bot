1f9f3bdf45497a109f2dfdc6168b8b4d7ac4fc35
andyyu <andyhsyu@gmail.com>
2015-12-23 20:16:22 -0800

    work up to game loop

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.common.exceptions import *
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from pystockfish import *
 import time
 
+driver = None
+
 def main():
+  global driver
   driver = webdriver.Chrome()
   driver.get("http://www.chess.com")
   #assert "Python" in driver.title
@@ -20,7 +25,36 @@ def main():
         print "Switched to latest window."
       continue
   print "Started game."
-  board = driver.find_element_by_id("chess_board")
+  play_game()
+
+def play_game():
+  turn_count = 0
+  game_state = {}
+  move_list = []
+  player_color = 1  # 1:black 0:white
+  ### color initialization ###
+  color_element = driver.find_element_by_xpath("//a[contains(@id, '_player_name_')]")
+  color = color_element.get_attribute("id")[:5] # color of the opponent
+  if (color == "black"):
+    player_color = 0
+  ### game loop ###
+  while True:  # TODO - replace true with endgame condition
+    board_element = driver.find_element_by_xpath("//div[contains(@id, '_boardarea')]")
+    squares_list = board_element.find_elements_by_tag_name("img")
+    board = {}
+    for square in squares_list[:-4]:
+      coord = square.get_attribute("id")[-2:]
+      board[coord] = square.get_attribute("src")[-5]
+    if (len(game_state) == 0):
+      game_state = board
+    diff_set = set(board.items()) ^ set(game_state.items())  # compare game state and current board
+    if (len(diff_set) != 0 or (turn_count + player_color == 0)): # move has been made, or first turn as white
+      print diff_set
+      if (turn_count % 2 == player_color):
+        print ""
+        # TODO - make move
+      turn_count += 1
+    game_state = board  # update gamestate
 
 if __name__ == "__main__":
     main()
